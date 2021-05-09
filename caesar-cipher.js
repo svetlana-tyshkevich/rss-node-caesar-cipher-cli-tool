66281ae12ea7e87b936ef3d7e166be8d579a1b4a
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2021-05-09 12:56:25 +0300

    fix: fix cipher function

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,15 +1,18 @@
-const caesarCipher = (text, shift) => {
+const caesarCipher = (text, shift, action) => {
   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const alphabetLength = alphabet.length / 2;
-  // const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  const codeShift = action === 'encode' ? shift : shift * -1;
 
   const getCipherSymbol = (symbol, shift, action) => {
     const index = alphabet.indexOf(symbol);
-    const codeShift = action === 'encode' ? shift : -shift;
     if (index < 0) return symbol;
     else if (index < alphabetLength) {
       return alphabet[(index + codeShift) % alphabetLength];
-    } else return alphabet[((index + codeShift) % alphabetLength) + alphabetLength];
+
+      console.log(alphabet[(index + codeShift) % alphabetLength]);
+    } else
+      return alphabet[((index + codeShift) % alphabetLength) + alphabetLength];
   };
 
   return text
@@ -19,5 +22,3 @@ const caesarCipher = (text, shift) => {
 };
 
 export default caesarCipher;
-
-
